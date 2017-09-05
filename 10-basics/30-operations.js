e48747a769a531a91556030bb1c2be5f2a95cf0e
Lukáš Čech <ja@lukascech.cz>
2017-09-05 12:16:32 +0200

    Presnejsi ukazka vystupu
    ...tedy jestli ty veci v komentarich mely ilustrovat vysledek

--- 10-basics/30-operations.js
+++ 10-basics/30-operations.js
@@ -24,13 +24,13 @@ console.log(5 * '1');        // parseFloat
 
 // to string
 
-console.log('a' + undefined);  // 'undefined'
-console.log('a' + null);       // 'null'
-console.log('a' + true);       // 'true'
-console.log('a' + false);      // 'false'
-console.log('a' + 123);        // '123'
-console.log('a' + {});         // '[object Object]'
-console.log('a' + []);         // ''
-console.log('a' + [1, 2, 3]);  // '1,2,3'
+console.log('a' + undefined);  // 'aundefined'
+console.log('a' + null);       // 'anull'
+console.log('a' + true);       // 'atrue'
+console.log('a' + false);      // 'afalse'
+console.log('a' + 123);        // 'a123'
+console.log('a' + {});         // 'a[object Object]'
+console.log('a' + []);         // 'a'
+console.log('a' + [1, 2, 3]);  // 'a1,2,3'
 
 
